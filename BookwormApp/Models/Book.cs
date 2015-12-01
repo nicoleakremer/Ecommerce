cabf6c03b43579ac0176e66f53e62a76ee31fbf6
nicoleakremer <nicoleakremer@student.cofo.edu>
2015-11-30 22:49:17 -0600

    Books

---
+++ BookwormApp/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookwormApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public int PublisherId { get; set; }
+        public Nullable<int> InventoryId { get; set; }
+        public string Title { get; set; }
+        public Nullable<int> Edition { get; set; }
+        public Nullable<int> Rating { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public Nullable<int> ISBN { get; set; }
+        public Nullable<System.DateTime> CopyRightDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        public decimal RetailPrice { get; set; }
+    
+    }
+}
---
+++ BookwormApp/Models/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookwormApp.Models
+{
+    public class Cart
+    {
+        public int BookId { get; set; }
+        public int PublisherId { get; set; }
+        public Nullable<int> InventoryId { get; set; }
+        public string Title { get; set; }
+        public Nullable<int> Edition { get; set; }
+        public Nullable<int> Rating { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public Nullable<int> ISBN { get; set; }
+        public Nullable<System.DateTime> CopyRightDate { get; set; }
+        public int CartId { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public decimal RetailPrice { get; set; }
+
+
+    }
+}
